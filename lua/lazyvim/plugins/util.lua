a49b1c3b1f895d9a4731bde9c31ea2f82c94ce6c
gaurav23617 <gaurav23617@gmail.com>
2025-03-30 08:07:28 +0530

    syntax error fixed in plugin util

--- lua/lazyvim/plugins/util.lua
+++ lua/lazyvim/plugins/util.lua
@@ -17,13 +17,13 @@ return {
       bigfile = { enabled = true },
       quickfile = { enabled = true },
       terminal = {
-        -- win = {
-        --   keys = {
-        --     nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
-        --     nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
-        --     nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
-        --     nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
-        --   },
+        win = {
+          keys = {
+            nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+            nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+            nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+            nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+          },
         },
       },
     },
