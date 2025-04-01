5cbc64a40b84fb1f50a62497a25e07ac5d15a852
gaurav23617 <gaurav23617@gmail.com>
2025-04-01 21:34:58 +0530

    copilot status reoved from ui lualine

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -137,39 +137,6 @@ return {
         },
         extensions = { "neo-tree", "lazy", "fzf" },
       }
-
-      -- Add Copilot status
-      table.insert(
-        opts.sections.lualine_x,
-        3,
-        LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
-          local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
-          if #clients > 0 then
-            local status = require("copilot.api").status.data.status
-            return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
-          end
-        end)
-      )
-      -- do not add trouble symbols if aerial is enabled
-      -- And allow it to be overriden for some buffer types (see autocmds)
-      if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
-        local trouble = require("trouble")
-        local symbols = trouble.statusline({
-          mode = "symbols",
-          groups = {},
-          title = false,
-          filter = { range = true },
-          format = "{kind_icon}{symbol.name:Normal}",
-          hl_group = "lualine_c_normal",
-        })
-        table.insert(opts.sections.lualine_c, {
-          symbols and symbols.get,
-          cond = function()
-            return vim.b.trouble_lualine ~= false and symbols.has()
-          end,
-        })
-      end
-
       return opts
     end,
   },
