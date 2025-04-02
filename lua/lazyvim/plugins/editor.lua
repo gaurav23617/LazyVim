7c4f3dd6118fcc325ed5e50fd385d6a5e56b876f
gaurav23617 <gaurav23617@gmail.com>
2025-04-02 21:15:33 +0530

    diffview removed form plugins editor

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -192,21 +192,6 @@ return {
     end,
   },
 
-  -- Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
-  {
-    "sindrets/diffview.nvim",
-    cmd = {
-      "DiffviewOpen",
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Open DiffView for git" },
-      { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Close DiffView for git" },
-    },
-  },
-
   -- better diagnostics list and others
   {
     "folke/trouble.nvim",
