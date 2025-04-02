07e54c0aa0e4e7fe641cdde85b7ccb63efd96877
gaurav23617 <gaurav23617@gmail.com>
2025-04-02 07:20:14 +0530

    snacks image added

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -224,6 +224,7 @@ return {
       indent = { enabled = true },
       input = { enabled = true },
       notifier = { enabled = true },
+      image = { enabled = true },
       scope = { enabled = true },
       scroll = { enabled = true },
       statuscolumn = { enabled = false }, -- we set this in options.lua
