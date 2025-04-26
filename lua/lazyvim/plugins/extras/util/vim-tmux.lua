e87a627dc9b19b9207c1abd6208a942101db4bef
gaurav23617 <gaurav23617@gmail.com>
2025-04-26 17:40:46 +0530

    vim-tmux removed form util and added in extras

---
+++ lua/lazyvim/plugins/extras/util/vim-tmux.lua
@@ -0,0 +1,19 @@
+return {
+  -- This plugin allow you to move between Vim panes and tmux splits seamlessly.
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
