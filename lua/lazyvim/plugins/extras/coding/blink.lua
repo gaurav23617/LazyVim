8f66f79e0ba5255e06140d9625772a5bd82d62eb
gaurav23617 <gaurav23617@gmail.com>
2025-06-03 23:05:00 +0530

    change blink fuzzy implementation to rust

--- lua/lazyvim/plugins/extras/coding/blink.lua
+++ lua/lazyvim/plugins/extras/coding/blink.lua
@@ -51,7 +51,7 @@ return {
         nerd_font_variant = "mono",
       },
       fuzzy = {
-        implementation = "lua",
+        implementation = "prefer_rust",
         use_frecency = true,
         use_unsafe_no_lock = false,
         sorts = {
